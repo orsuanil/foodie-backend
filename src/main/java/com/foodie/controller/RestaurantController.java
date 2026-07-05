df14f0784d259269b6577b99f51ed6b37e50bb15
Orsu Anil <orsuorsuanil272@gmail.com>
2026-07-05 21:41:03 +0530

    Allow Netlify frontend

--- src/main/java/com/foodie/controller/RestaurantController.java
+++ src/main/java/com/foodie/controller/RestaurantController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://127.0.0.1:5500")
+@CrossOrigin(origins = {
+        "http://127.0.0.1:5500",
+        "https://foodie-anil.netlify.app"
+})
 @RequestMapping("/restaurants")
 public class RestaurantController {
 
